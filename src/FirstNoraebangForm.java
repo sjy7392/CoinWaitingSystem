3616998fd767fea2453f9ce46f7e29621442d39f
Jiyeon <sjy7392@naver.com>
2022-11-23 09:11:26 +0900

    20221122 NoraebangForm UI 설계 LoginForm main 코드 기능 Dashboard로 이동

--- src/FirstNoraebangForm.java
+++ src/FirstNoraebangForm.java
@@ -1,5 +1,13 @@
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 //textField로 data 입력받아 DB에 값 저장한 후 list로 출력
 //*고려사항*
 //
@@ -20,6 +28,92 @@ public class FirstNoraebangForm extends JFrame {
         setSize(1200, 700);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
+        createTable();
+        btnAdd.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                registerNoraebangUser();
+                //createTable();  //JTable에 출력
+            }
+        });
+    }
+
+    private void registerNoraebangUser(){
+        String name = tfName.getText();
+        String phone = tfPhone.getText();
+        String NumberOfPersons = tfNumberOfPersons.getText();
+        String time = tfTime.getText();
+
+        if(name.isEmpty()||phone.isEmpty()||NumberOfPersons.isEmpty()||time.isEmpty()){
+            JOptionPane.showMessageDialog(this, "Please enter all fields");
+            return;
+        }
+
+        JOptionPane.showMessageDialog(null, "Successfully Added");
+        noraebangUser = addNoraebangUserToDatabase(name, phone, NumberOfPersons, time); //db에 추가
+        createTable(); //JTable에 data 출력하기
+    }
+
+    public NoraebangUser noraebangUser;
+    private NoraebangUser addNoraebangUserToDatabase(String name, String phone, String NumberOfPersons, String time){
+        NoraebangUser noraebangUser = null;
+        final String DB_URL = "jdbc:mysql://localhost:3306/MyStore";
+        final String USERNAME = "root";
+        final String PASSWORD = "7392";
+
+        try {
+            Connection conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+
+            Statement stmt = conn.createStatement();
+            String sql = "INSERT INTO Noraebangusers (name, phone, NumberOfPersons, time) " +
+                    "VALUES (?, ?, ?, ?)";
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, phone);
+            preparedStatement.setString(3, NumberOfPersons);
+            preparedStatement.setString(4, time);
+
+            int addedRows = preparedStatement.executeUpdate();
+            if (addedRows > 0) {
+                noraebangUser = new NoraebangUser();
+                noraebangUser.name = name;
+                noraebangUser.phone = phone;
+                noraebangUser.NumberOfPersons = NumberOfPersons;
+                noraebangUser.time = time;
+            }
+            stmt.close();
+            conn.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return noraebangUser;
+    }
+
+    private void createTable(){
+
+        final String DB_URL = "jdbc:mysql://localhost:3306/MyStore";
+        final String USERNAME = "root";
+        final String PASSWORD = "7392";
+
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            String sql = "SELECT name, phone, numberofpersons, time FROM mystore.noraebangusers";
+            Connection conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+            pstmt = conn.prepareStatement(sql);
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                Object[][] data = {{rs.getString("name"), rs.getString("phone"), rs.getString("numberofpersons"), rs.getString("time")}};
+                String[] columnNames = {"Name","Phone","numberofpersons","time"};
+                showTable = new JTable(data,columnNames);
+        }
+
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
@@ -28,6 +122,7 @@ public class FirstNoraebangForm extends JFrame {
             public void run() {
                 try {
                     FirstNoraebangForm window = new FirstNoraebangForm();
+                    //NoraebangUser noraebangUser = window.noraebangUser;
                     window.setVisible(true);
                 } catch (Exception e) {
                     e.printStackTrace();
---
+++ src/NoraebangUser.java
@@ -0,0 +1,6 @@
+public class NoraebangUser {
+    public String name;
+    public String phone;
+    public String NumberOfPersons;
+    public String time;
+}
