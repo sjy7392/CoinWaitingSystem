e978ddbfa6cb623d25bf637a31829b08cc23705b
qorrnsu@gmail.com <qorrnsu@gmail.com>
2022-12-08 22:36:38 +0900

    1. 대시보드에 매니저 로그인 버튼 추가함 2. DB에 (백우정, 손지연, 김연수) 매니저 추가하고, 각 매니저가 로그인하면 각 노래방 예약페이지로 바로 연결됨.

--- src/DashboardForm.java
+++ src/DashboardForm.java
@@ -2,12 +2,15 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Objects;
 
 public class DashboardForm extends JFrame {
     private JPanel dashboardPanel;
     private JLabel lbAdmin;
     private JButton btnUserLogin;
     private JButton btnUserRegister;
+    private JButton btnAdminLogin;
+
 
     public DashboardForm(){
         setTitle("Dashboard");
@@ -17,6 +20,7 @@ public class DashboardForm extends JFrame {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
 
+        // 사용자 로그인 버튼
         btnUserLogin.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -32,12 +36,41 @@ public class DashboardForm extends JFrame {
             }
         });
 
+        // 사용자 회원가입 버튼
         btnUserRegister.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 RegisterForm registerForm = new RegisterForm(DashboardForm.this);
             }
         });
+
+        // 노래방 관리자 로그인 버튼
+        btnAdminLogin.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ManagerLogin managerlogin = new ManagerLogin(DashboardForm.this);
+                Manager manager = ManagerLogin.manager;
+                if (manager != null) {  //로그인 성공
+                    if (Objects.equals(manager.email, "qorrnsu00@naver.com")) {
+                        FirstNoraebangForm window = new FirstNoraebangForm();
+                        managerlogin.setVisible(false);
+                    }
+                    else if (Objects.equals(manager.email, "jiyeon@naver.com")) {
+                        SecondNoraebangForm window = new SecondNoraebangForm();
+                        managerlogin.setVisible(false);
+                    }
+                    else {
+                        ThirdNoraebangForm window = new ThirdNoraebangForm();
+                        managerlogin.setVisible(false);
+                    }
+
+                }
+                else {  //로그인 실패
+                    JOptionPane.showMessageDialog(null, "Authentication canceled");
+                }
+            }
+        });
+
     }
 
     public static void main(String[] args){
